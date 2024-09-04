37d85224d1452d7151bb021e3cbc4769f438c761
Devesh <deveshgupta123456789@gmail.com>
2024-09-04 14:01:35 +0530

    Adding Multiply

---
+++ multiply.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+    std::cout<<'Enter a number:' ;
+    double a ; 
+    std::cout<<"Enter another number: " ;
+    double b ; 
+    std::cout<<"The Multiplication of "<< a <<" and " << b <<'is: ' << a * b ; 
+    return 0  ;
+}
